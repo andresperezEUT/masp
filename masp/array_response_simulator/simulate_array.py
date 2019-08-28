7d452e8307c5b2d0a453f410e73dc2a58713ac48
Andres Pérez López <andres.perez@eurecat.org>
2019-08-28 21:07:23 +0200

    get_array_response docstring

--- masp/array_response_simulator/simulate_array.py
+++ masp/array_response_simulator/simulate_array.py
@@ -54,7 +54,7 @@ def simulate_sph_array(N_filt, mic_dirs_rad, src_dirs_rad, arrayType, R, N_order
         Expressed in [azi, ele] pairs. Dimension = (N_mic, C-1).
     src_dirs_rad: ndarray
         Direction of arrival of the indicent plane waves, in radians.
-         Expressed in [azi, ele] pairs. Dimension = (N_doa, C-1).
+        Expressed in [azi, ele] pairs. Dimension = (N_doa, C-1).
     arrayType: str
         'open', 'rigid' or 'directional'.
         Target sampling rate
@@ -220,67 +220,98 @@ def simulate_cyl_array(N_filt, mic_dirs_rad, src_dirs_rad, arrayType, R, N_order
     return h_mic, H_mic
 
 
-    # todo lffilt even
 def get_array_response(U_doa, R_mic, Lfilt, fs=48000, U_orient=None, fDir_handle=None):
     """
     Return array response of directional sensors.
-%
-%   GETARRAYRESPONSE computes the impulse responses of the microphones of
-%   an open array of directional microphones, located at R_mic and with
-%   orientations U_orient, for the directions-of-incidence U_doa. Each
-%   sensors directivity in defined by a function handle in the cell array
-%   fDir_handle.
-%
-% U_doa:    Mx3 matrix of vectors specifying the directions to evaluate the
-%           impulse response
-% R_mic:    Nx3 matrix of vectors specifying the positions of the microphones
-% U_orient: Nx3 matrix of unit vectors specifying the orientation of the
-%           microphones, or 1x3 unit vector if the microphones all have the
-%           same orientation. If left empty or not defined then the
-%           orientation of the microphones is assumed to be radial from the
-%           origin.
-% fDir_hadle:   1xN cell array of function handles describing the directivity
-%               of each sensor with respect the sensor's orientation and the
-%               DOA. The function considers only axisymmetric directivities,
-%               and the function should be of the form f(angle), where
-%               angle is the angle between the sensors orientation and the
-%               DOA. If a single function handle is provided, the same is
-%               applied to all sensors.
-% Lfilt:    Length of filter in samples.
-% fs:       Sampling rate. If empty or undefined, 48kHz are used.
-%
-%
-% micTFs:   Output the frequency responses for M directions for the N
-%           microphones. Only half the FFT spectrum is returned (up to Nyquist).
-%           Last dimension is the DOA.
-% micIRs:   Output the IRs for M directions for the N microphones. Last
-%           dimension is the DOA.
-%
-%   EXAMPLE - Simulate the response of a 3-microphone array of an
-%   omnidirectional microphone, a first-order cardioid and a second-order
-%   cardioid, with random locations and orientations, for front and side
-%   incidence:
-%
-%   Nmic = 3;
-%   U_doa = [1 0 0; 0 1 0];
-%   R_mic = rand(Nmic,3);
-%   U_orient = rand(Nmic,3);
-%   U_orient = U_orient./(sqrt(sum(U_orient.^2,2))*ones(1,3));   % convert to unit vectors
-%
-%   fdir_omni = @(angle) ones(size(angle));
-%   fdir_card = @(angle) (1/2)*(1 + cos(angle));
-%   fdir_card2 = @(angle) (1/2)^2 * (1 + cos(angle)).^2;
-%   fDir_handle = {fdir_omni, fdir_card, fdir_card2};
-%
-%   [micIRs, micTFs] = getArrayResponse(U_doa, R_mic, U_orient, fDir_handle);
-%
-%   plot(micIRs(:,:,1))
-%   legend('omni','1st-order cardioid','2nd-order cardioid')
-%
+
+    The function computes the impulse responses of the microphones of
+    an open array of directional microphones, located at R_mic and with
+    orientations U_orient, for the directions-of-incidence U_doa. Each
+    sensors directivity in defined by a function handle in the cell array
+    fDir_handle.
+
+    Parameters
+    ----------
+    U_doa: ndarray
+        Direction of arrival of the indicent plane waves, in cartesian coordinates.
+        Dimension = (Ndoa, C).
+    R_mic: ndarray
+        Position of microphone capsules, in cartesian coordinates.
+        Dimension = (Nmic, C).
+    Lfilt: int
+        Number of frequencies where to compute the responses. It must be even.
+    fs: int, optional.
+        Sample rate. Default to 48000 Hz.
+    U_orient: optional.
+        Position of microphone capsules, in cartesian coordinates.
+        Default to None. See notes.
+    fDir_handle: optional.
+        Microphone directivity functions.
+        Default to None. See notes.
+
+    Returns
+    -------
+    micIRs: ndarray
+        Computed IRs in time-domain. Dimension = (Lfilt, Nmic, Ndoa)
+    micTFs: ndarray, dtype='complex'
+        Frequency responses of the computed IRs. Dimension = (Lfilt//2+1, N_mic, N_doa).
+
+    Raises
+    -----
+    TypeError, ValueError: if method arguments mismatch in type, dimension or value.
+
+    Notes
+    -----
+    In the general case, `U_orient` defines the orientation (in cartesian) of each
+    microphone capsule. Therefore, it is expected to be a 2D ndarray with dimension (Nmic, C).
+    It is possible to define the same orientation for all capsules. In this case,
+    `U_orient` is expected to be a 1D array of length C.
+    If `U_orient` is left unespecified, the orientation of the microphones is assumed
+    to be radial from the origin.
+
+    `fDir_hadle` allows the specification of custom capsulse directivity polar patterns,
+    expressed as lambda expressions with one parameter (lambda angle: f(angle)).
+    In the general case, `fDir_hadle` is expected to be a 1D ndarray of length `Nmic`,
+    with each lambda corresponding to the directivity of a microphone capsule.
+    It is possible to define the directivity for all capsules. In this case,
+    `fDir_hadle` is expected to be a lambda expression.
+    If `fDir_hadle` is left unespecified, the function assumes omnidirectional direcivities
+    for all capsules.
+
+
+    Examples
+    -----
+    Simulate the response of a 3-microphone array of an omnidirectional microphone,
+    a first-order cardioid and a second-order cardioid, with random locations and
+    orientations, for front and side incidence:
+
+    Nmic = 3
+    Lfilt = 1000
+    U_doa = np.asarray([[1, 0, 0],[0, 1, 0]])
+    R_mic = np.random.rand(Nmic,3)
+    U_orient = np.random.rand(Nmic,3)
+    U_orient/np.tile(np.sqrt(np.sum(np.power(U_orient,2),axis=1)), (3,1)).T # Unit vector
+
+    fdir_omni = lambda angle: np.ones(np.size(angle))
+    fdir_card = lambda angle: (1/2)*(1 + np.cos(angle));
+    fdir_card2 = lambda angle: np.power(1/2,2) * np.power((1 + np.cos(angle)),2)
+    fDir_handle = np.asarray([fdir_omni, fdir_card, fdir_card2])
+
+    micIRs, micTFs = ars.get_array_response(U_doa, R_mic, U_orient=U_orient,
+                                            fDir_handle=fDir_handle, Lfilt=Lfilt)
+
+    import matplotlib.pyplot as plt
+    plt.plot(micIRs[:,:,0])
+    plt.show()
     """
 
-    Nmics = R_mic.shape[0]
     Ndoa = U_doa.shape[0]
+    Nmics = R_mic.shape[0]
+    _validate_ndarray_2D('U_doa', U_doa, shape1=masp.utils.C)
+    _validate_ndarray_2D('R_mic', R_mic, shape1=masp.utils.C)
+    _validate_int('Lfilt', Lfilt, positive=True, parity='even')
+    _validate_int('fs', fs, positive=True)
+
 
     # If no directivity coefficient is defined assume omnidirectional sensors
     if fDir_handle is None:
@@ -294,7 +325,7 @@ def get_array_response(U_doa, R_mic, Lfilt, fs=48000, U_orient=None, fDir_handle
             for i in range(Nmics):
                 assert masp.utils.islambda(fDir_handle[i])
 
-    # Compute unit vectors of the microphone positions
+    # Compute unit vectors of the microphone positionsT
     normR_mic = np.sqrt(np.sum(np.power(R_mic, 2), axis=1))
     U_mic = R_mic / normR_mic[:, np.newaxis]
 
@@ -309,6 +340,7 @@ def get_array_response(U_doa, R_mic, Lfilt, fs=48000, U_orient=None, fDir_handle
             U_orient = np.tile(U_orient, (Nmics, 1))
         else:
             _validate_ndarray_2D('U_orient', U_orient, shape1=masp.utils.C)
+
 
     # Frequency vector
     Nfft = Lfilt
